1d4dd1539d70c2fca5f09307fd7ebe6bbb968750
mariem louati <louaty.mariemm@gmail.com>
2026-04-10 12:28:49 +0100

    Ajout Cloudinary

--- blogproject/settings_production.py
+++ blogproject/settings_production.py
@@ -19,6 +19,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'cloudinary_storage',
+    'cloudinary',
+    'shop',
 ]
 
 MIDDLEWARE = [
@@ -88,3 +91,16 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ✅ CLOUDINARY
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
