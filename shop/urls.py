dd7c6efde8709404407ba83d6f4e7ad43f2e2ec7
mariem-louati <louaty.mariemm@gmail.com>
2026-04-13 22:33:35 +0100

    Refactor URLs by removing comments and adding newline
    Removed comments for better readability and added a newline at the end of the file.

--- shop/urls.py
+++ shop/urls.py
@@ -2,19 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # 🏠 Accueil
     path('', views.product_list, name='product_list'),
-
-    # 📂 Catégories
     path('categories/', views.category_list, name='category_list'),
     path('category/<int:category_id>/', views.products_by_category, name='products_by_category'),
-
-    # 🛒 Panier
     path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart_view, name='cart'),
     path('remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('clear/', views.clear_cart, name='clear_cart'),
     path('increase/<int:product_id>/', views.increase_quantity, name='increase_quantity'),
     path('decrease/<int:product_id>/', views.decrease_quantity, name='decrease_quantity'),
-    path('debug/', views.debug_images, name='debug_images'),path('migrate/', views.migrate_images, name='migrate_images'),
+    path('debug/', views.debug_images, name='debug_images'),
 ]
